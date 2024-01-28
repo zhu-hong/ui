282a0e3ab1c09cc89222d1f69cff2e493d3f726f
zh <zhxhyy@qq.com>
2024-01-28 19:24:19 +0800

    table try

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
+import unocss from 'unocss/vite'
+import { presetWind } from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    unocss({
+      preflights: false,
+      presets: [
+        presetWind({
+          preflight: false,
+        }),
+      ],
+    }),
   ],
   resolve: {
     alias: {
