a4f4ce24e6fd235e2fdd7423e1c20d6cbb88956b
zh <zhxhyy@qq.com>
2024-02-26 11:25:55 +0800

    cls

--- src/select/select.tsx
+++ src/select/select.tsx
@@ -23,7 +23,7 @@ export const Select: FC<
       }}
       menuItemSelectedIcon={<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48"><path fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="4" d="m4 24l5-5l10 10L39 9l5 5l-25 25z"></path></svg>}
       removeIcon={<IconButton tabIndex={-1} sx={{padding:'4px',flex:'none'}}><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12"><path fill="#7F848B" fillRule="evenodd" d="M3.948.896a5.5 5.5 0 0 1 5.94 1.216 5.49 5.49 0 0 1 1.194 5.995A5.49 5.49 0 0 1 6 11.5a5.5 5.5 0 0 1-3.889-1.606A5.5 5.5 0 0 1 .5 6v-.11A5.5 5.5 0 0 1 3.948.896m3.347 3.105L6 5.296 4.705 4 4 4.706 5.295 6 4 7.296 4.705 8 6 6.706 7.295 8 8 7.296 6.705 6 8 4.706z"/></svg></IconButton>}
-      placeholder={other.placeholder??'请选择'}
+      placeholder={other.placeholder===undefined?'请选择':other.placeholder}
       showSearch={other.showSearch||false}
       listHeight={other.listHeight||520}
       dropdownMatchSelectWidth={other.dropdownMatchSelectWidth||false}
