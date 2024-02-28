302c22192cfe4c2ed0230159da5b974a818493ac
zh <zhxhyy@qq.com>
2024-02-28 21:48:44 +0800

    cls

--- src/dialog/dialog.tsx
+++ src/dialog/dialog.tsx
@@ -1,6 +1,5 @@
-import { type FC, useState } from 'react'
+import { type FC } from 'react'
 import RcDialog, { type DialogProps } from 'rc-dialog'
-import { render, unmount } from 'rc-util/es/React/render'
 
 import './dialog.css'
 
@@ -12,31 +11,4 @@ export const Dialog: FC<Omit<DialogProps, 'visible'|'prefixCls'|'animation'|'mas
     animation='slide'
     maskAnimation='fade'
   />
-}
-
-const ConfirmDialog = ({ afterClose, resolve, reject }) => {
-  const [open, setOpen] = useState(true)
-
-  return <Dialog title='提示' open={open} onClose={() => setOpen(false)} footer={<>
-    <button onClick={() => {
-      resolve()
-      setOpen(false)
-    }}>同意</button>
-    <button onClick={() => {
-      reject()
-      setOpen(false)
-    }}>不同意</button>
-  </>} afterClose={afterClose}>
-    这是☝️提示
-  </Dialog>
-}
-
-export const confirm = () => {
-  return new Promise((resolve, reject) => {
-    const container = document.createDocumentFragment()
-
-    render(<ConfirmDialog afterClose={() => {
-      unmount(container)
-    }} resolve={resolve} reject={reject} />, container)
-  })
 }
